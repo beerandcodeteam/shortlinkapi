26f329833147ffc1ceecc8ff6dbfa45f1889279d
Lucas Silva <lucas.roberto@creditas.com>
2024-05-18 07:25:18 -0300

    remove routes

--- routes/api.php
+++ routes/api.php
@@ -7,14 +7,3 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
-Route::post('/login', [\App\Http\Controllers\LoginController::class, 'login'])
-    ->middleware('guest')
-    ->name('login');
-
-Route::post('/shortlink', [\App\Http\Controllers\ShortLinkController::class, 'store'])
-    ->middleware('auth:sanctum')
-    ->name('shortlink');
-
-Route::get('/static', [\App\Http\Controllers\ShortLinkController::class, 'static'])
-    ->name('static');
