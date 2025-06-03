21b889d45f882234e78940b2971f2505df83d799
Lucas Souza <lucasrsouzasilva@gmail.com>
2025-06-03 07:37:14 -0300

    add api routes

--- routes/api.php
+++ routes/api.php
@@ -3,20 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
 
-Route::post('/login', [\App\Http\Controllers\LoginController::class, 'login'])
-    ->middleware('guest')
-    ->name('login');
+//Route::post('/login', [\App\Http\Controllers\LoginController::class, 'login'])
+//    ->middleware('guest')
+//    ->name('login');
 
 Route::get('/{name}', [\App\Http\Controllers\ShortlinkController::class, 'show'])
-    ->name('shortlink.show');
+    ->name('shortlink.showcache');
 
 Route::get('/{name}/no-cache', [\App\Http\Controllers\ShortlinkController::class, 'showLink'])
     ->name('shortlink.nocache');
 
 
-Route::get('/static', [\App\Http\Controllers\ShortlinkController::class, 'static'])
-    ->name('shortlink.static');
+//Route::get('/static', [\App\Http\Controllers\ShortlinkController::class, 'static'])
+//    ->name('shortlink.static');
