0a4b1a7a1590273704ec331d4f00f4014ad73448
Lucas Souza <lucasrsouzasilva@gmail.com>
2025-06-03 07:37:48 -0300

    add api routes

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/user', function (Request $request) {
-//    return $request->user();
-//})->middleware('auth:sanctum');
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
-//Route::post('/login', [\App\Http\Controllers\LoginController::class, 'login'])
-//    ->middleware('guest')
-//    ->name('login');
+Route::post('/login', [\App\Http\Controllers\LoginController::class, 'login'])
+    ->middleware('guest')
+    ->name('login');
 
 Route::get('/{name}', [\App\Http\Controllers\ShortlinkController::class, 'show'])
     ->name('shortlink.showcache');
@@ -18,5 +18,5 @@ Route::get('/{name}/no-cache', [\App\Http\Controllers\ShortlinkController::class
     ->name('shortlink.nocache');
 
 
-//Route::get('/static', [\App\Http\Controllers\ShortlinkController::class, 'static'])
-//    ->name('shortlink.static');
+Route::get('/static', [\App\Http\Controllers\ShortlinkController::class, 'static'])
+    ->name('shortlink.static');
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,3 @@ Route::get('/{name}', [\App\Http\Controllers\ShortlinkController::class, 'show']
     ->middleware('guest')
     ->name('shortlink.show');
 
-Route::get('/{name}/no-cache', [\App\Http\Controllers\ShortlinkController::class, 'showLink'])
-    ->middleware('guest')
-    ->name('shortlink.nocache');
